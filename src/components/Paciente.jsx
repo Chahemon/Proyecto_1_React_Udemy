aecf4b5d9c20f34b25776b0af2b6e72ecbed3b21
HectorChavez <hectormcdlv@gmail.com>
2022-09-05 20:40:17 -0500

    agregamos useEffect

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,6 @@
 
 
-const Paciente = ({paciente}) => {
+const Paciente = ({paciente, setPaciente}) => {
 
   const {nombre, propietario, email, fecha, sintomas} = paciente
 
@@ -31,6 +31,7 @@ const Paciente = ({paciente}) => {
           <button type="button" 
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold
                       uppercase rounded-lg"
+                      onClick={() => setPaciente(paciente)}
           >Editar</button>
 
           <button type="button"
